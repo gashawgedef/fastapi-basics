c097cdee84c20d7912d23a91ffeb6073918c32ff
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-08 14:32:27 -0700

    part two

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
+
+#This is comment
+#Path Parameter with type
+@app.get('/gashity/{id}')
+async def get_list(id:int):
+    return {"The id number is:":id}
